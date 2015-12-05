7f9faa8993cf37ec97a551873d22e97270c207cf
Bahamut_x2 <esteban.savia@gmail.com>
2015-12-05 12:47:04 -0500

    ServidorRest

---
+++ WepAppiConMetodos/Controllers/CiudadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WepAppiConMetodos.ServicioRest;
+
+namespace WepAppiConMetodos.Controllers
+{
+    public class CiudadController : ApiController
+    {
+        ServiciosMovil sm = new ServiciosMovil();
+
+        public List<string> GetObtenerCategorias()
+        {
+            List<string> l = new List<string>();
+            string[] cate = sm.ObtenerCategorias();
+            foreach (string a in cate)
+            {
+                l.Add(a);
+            }
+            return l;
+        }
+
+        [HttpPost]
+        public string PostSubirFoto(string latitud, string longitud, string imageb64, string categoria)
+        {
+            return "a";
+            //return sm.CrearDenunciaPorFoto(latitud.Trim(), longitud.Trim(), categoria, imageb64);
+        }
+    }
+}
---
+++ WepAppiConMetodos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WepAppiConMetodos.WebApiApplication" Language="C#" %>
